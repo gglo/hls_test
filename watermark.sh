33afb433ba8d7d683609117d8b8fab67a580e76d
GangWang <gang.wang@zoom.us>
2022-08-26 09:20:11 +0800

    watermark

--- watermark.sh
+++ watermark.sh
@@ -3,10 +3,12 @@
 
 function watermark(){
 
+name=$(basename "$1")
+dir=$(dirname "$1")
 echo "源文件为："$1
 echo "水印文件为："$2
-ffmpeg -i "$1" -i "$2" -filter_complex "overlay=x=0:y=0" source_watermark/watermark.mp4
-echo "水印添加完成"
+ffmpeg -i "$1" -i "$2" -filter_complex "overlay=x=W-w:y=0" "$dir/${name%.*}-watermark.mp4"
+echo "水印添加完成"$dir/${name%.*}-watermark.mp4""
 
 }
 
